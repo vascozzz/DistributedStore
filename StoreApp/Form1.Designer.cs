90c83b6f26f61329f02575f5305d2efa22b1575d
Xkynar <xkynar@gmail.com>
2015-05-21 16:47:34 +0100

    Created Store Application
    Already fetches book list and displays it

---
+++ StoreApp/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace StoreApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.bookIdField = new MetroFramework.Controls.MetroTextBox();
+            this.bookQuantityField = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.clientAddressField = new MetroFramework.Controls.MetroTextBox();
+            this.clientNameField = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.clientEmailField = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel8 = new MetroFramework.Controls.MetroLabel();
+            this.orderSubmitBtn = new MetroFramework.Controls.MetroButton();
+            this.bookGrid = new MetroFramework.Controls.MetroGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.bookGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(13, 42);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(74, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Add Order";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(14, 108);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(23, 19);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "Id:";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(14, 137);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(61, 19);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.Text = "Quantity:";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(14, 81);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(111, 19);
+            this.metroLabel4.TabIndex = 3;
+            this.metroLabel4.Text = "Book Information";
+            // 
+            // bookIdField
+            // 
+            this.bookIdField.Lines = new string[0];
+            this.bookIdField.Location = new System.Drawing.Point(81, 108);
+            this.bookIdField.MaxLength = 32767;
+            this.bookIdField.Name = "bookIdField";
+            this.bookIdField.PasswordChar = '\0';
+            this.bookIdField.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.bookIdField.SelectedText = "";
+            this.bookIdField.Size = new System.Drawing.Size(75, 23);
+            this.bookIdField.TabIndex = 4;
+            this.bookIdField.UseSelectable = true;
+            // 
+            // bookQuantityField
+            // 
+            this.bookQuantityField.Lines = new string[0];
+            this.bookQuantityField.Location = new System.Drawing.Point(81, 137);
+            this.bookQuantityField.MaxLength = 32767;
+            this.bookQuantityField.Name = "bookQuantityField";
+            this.bookQuantityField.PasswordChar = '\0';
+            this.bookQuantityField.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.bookQuantityField.SelectedText = "";
+            this.bookQuantityField.Size = new System.Drawing.Size(75, 23);
+            this.bookQuantityField.TabIndex = 7;
+            this.bookQuantityField.UseSelectable = true;
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(14, 181);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(114, 19);
+            this.metroLabel5.TabIndex = 8;
+            this.metroLabel5.Text = "Client Information";
+            // 
+            // clientAddressField
+            // 
+            this.clientAddressField.Lines = new string[0];
+            this.clientAddressField.Location = new System.Drawing.Point(81, 243);
+            this.clientAddressField.MaxLength = 32767;
+            this.clientAddressField.Name = "clientAddressField";
+            this.clientAddressField.PasswordChar = '\0';
+            this.clientAddressField.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.clientAddressField.SelectedText = "";
+            this.clientAddressField.Size = new System.Drawing.Size(75, 23);
+            this.clientAddressField.TabIndex = 12;
+            this.clientAddressField.UseSelectable = true;
+            // 
+            // clientNameField
+            // 
+            this.clientNameField.Lines = new string[0];
+            this.clientNameField.Location = new System.Drawing.Point(81, 214);
+            this.clientNameField.MaxLength = 32767;
+            this.clientNameField.Name = "clientNameField";
+            this.clientNameField.PasswordChar = '\0';
+            this.clientNameField.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.clientNameField.SelectedText = "";
+            this.clientNameField.Size = new System.Drawing.Size(75, 23);
+            this.clientNameField.TabIndex = 11;
+            this.clientNameField.UseSelectable = true;
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(14, 243);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(59, 19);
+            this.metroLabel6.TabIndex = 10;
+            this.metroLabel6.Text = "Address:";
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.Location = new System.Drawing.Point(14, 214);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(52, 19);
+            this.metroLabel7.TabIndex = 9;
+            this.metroLabel7.Text = "Name: ";
+            // 
+            // clientEmailField
+            // 
+            this.clientEmailField.Lines = new string[0];
+            this.clientEmailField.Location = new System.Drawing.Point(81, 272);
+            this.clientEmailField.MaxLength = 32767;
+            this.clientEmailField.Name = "clientEmailField";
+            this.clientEmailField.PasswordChar = '\0';
+            this.clientEmailField.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.clientEmailField.SelectedText = "";
+            this.clientEmailField.Size = new System.Drawing.Size(75, 23);
+            this.clientEmailField.TabIndex = 14;
+            this.clientEmailField.UseSelectable = true;
+            // 
+            // metroLabel8
+            // 
+            this.metroLabel8.AutoSize = true;
+            this.metroLabel8.Location = new System.Drawing.Point(14, 272);
+            this.metroLabel8.Name = "metroLabel8";
+            this.metroLabel8.Size = new System.Drawing.Size(41, 19);
+            this.metroLabel8.TabIndex = 13;
+            this.metroLabel8.Text = "Email";
+            // 
+            // orderSubmitBtn
+            // 
+            this.orderSubmitBtn.Location = new System.Drawing.Point(14, 318);
+            this.orderSubmitBtn.Name = "orderSubmitBtn";
+            this.orderSubmitBtn.Size = new System.Drawing.Size(142, 23);
+            this.orderSubmitBtn.TabIndex = 15;
+            this.orderSubmitBtn.Text = "Submit Order";
+            this.orderSubmitBtn.UseSelectable = true;
+            // 
+            // bookGrid
+            // 
+            this.bookGrid.AllowUserToResizeRows = false;
+            this.bookGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.bookGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.bookGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.bookGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.bookGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.bookGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.bookGrid.DefaultCellStyle = dataGridViewCellStyle2;
+            this.bookGrid.EnableHeadersVisualStyles = false;
+            this.bookGrid.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.bookGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.bookGrid.Location = new System.Drawing.Point(208, 42);
+            this.bookGrid.MultiSelect = false;
+            this.bookGrid.Name = "bookGrid";
+            this.bookGrid.ReadOnly = true;
+            this.bookGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.bookGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.bookGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.bookGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.bookGrid.Size = new System.Drawing.Size(446, 303);
+            this.bookGrid.TabIndex = 16;
+            this.bookGrid.SelectionChanged += new System.EventHandler(this.bookGrid_SelectionChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(677, 368);
+            this.Controls.Add(this.bookGrid);
+            this.Controls.Add(this.orderSubmitBtn);
+            this.Controls.Add(this.clientEmailField);
+            this.Controls.Add(this.metroLabel8);
+            this.Controls.Add(this.clientAddressField);
+            this.Controls.Add(this.clientNameField);
+            this.Controls.Add(this.metroLabel6);
+            this.Controls.Add(this.metroLabel7);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.bookQuantityField);
+            this.Controls.Add(this.bookIdField);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Name = "Form1";
+            this.Resizable = false;
+            ((System.ComponentModel.ISupportInitialize)(this.bookGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroTextBox bookIdField;
+        private MetroFramework.Controls.MetroTextBox bookQuantityField;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroTextBox clientAddressField;
+        private MetroFramework.Controls.MetroTextBox clientNameField;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroTextBox clientEmailField;
+        private MetroFramework.Controls.MetroLabel metroLabel8;
+        private MetroFramework.Controls.MetroButton orderSubmitBtn;
+        private MetroFramework.Controls.MetroGrid bookGrid;
+    }
+}
+
